c49e974461da6bbda7d54bceafa1a7dd4e2e64bc
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-01-28 07:30:45 +0100

    more doc

--- Terradue.ServiceModel.Ogc.OwsContext/Terradue/ServiceModel/Ogc/OwsModel/OwcContent.cs
+++ Terradue.ServiceModel.Ogc.OwsContext/Terradue/ServiceModel/Ogc/OwsModel/OwcContent.cs
@@ -3,10 +3,10 @@
 namespace Terradue.ServiceModel.Ogc.OwsModel {
 
     /// <summary>
-    /// Content
+    /// OWC Content
     /// </summary>
     /// <description>
-    /// This object is a generic container for any content
+    /// This object is a generic container for any content in a \OWSContext Document.
     /// </description>
     /// \xrefitem rmodp "RM-ODP" "RM-ODP Doc"
     /// \ingroup OWSContext
