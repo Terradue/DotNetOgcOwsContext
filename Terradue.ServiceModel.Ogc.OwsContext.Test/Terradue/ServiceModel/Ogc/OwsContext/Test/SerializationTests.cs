0f472f6805a2bbf86ab4cdaef1a7cff4cb4c8614
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2019-03-21 17:00:16 +0100

    d
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

---
+++ Terradue.ServiceModel.Ogc.OwsContext.Test/Terradue/ServiceModel/Ogc/OwsContext/Test/SerializationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Linq;
+using Terradue.OpenSearch.Result;
+
+namespace Terradue.ServiceModel.Ogc.Owc.AtomEncoding.Test {
+
+    [TestFixture]
+    public class SerializationTests {
+
+        [Test]
+        public void DateExtensions() {
+
+			var feed = AtomFeed.Load(XmlReader.Create(File.OpenRead("../in/offeringdateext.xml")));
+
+			OwsContextAtomFeed ows = new OwsContextAtomFeed(feed.Feed, true);
+
+			SerializationTest.SerializeToStream(ows, Console.OpenStandardOutput());
+
+        }
+    }
+}
+
